5ffbc05558a0b5bb228baba9e0d9fc2372a4ce47
DaichiNakagama <nakadaichi@icloud.com>
2020-10-21 03:04:13 +0900

    stateへんこう

--- C`lock/Assets/Script/CameraTrack.cs
+++ C`lock/Assets/Script/CameraTrack.cs
@@ -7,15 +7,23 @@ public class CameraTrack : MonoBehaviour
     [SerializeField]
     GameObject _player;
     Vector3 _offset;
-    // Start is called before the first frame update
+    float _rotate;
     void Start()
     {
         _offset = new Vector3(0, transform.position.y, transform.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        _rotate = Input.GetAxis("Horizontal_R");
+        // _rotate = Input.GetAxis("Vertical_R");
+        AdjustmentPosition();
         transform.position = _player.transform.position + _offset;
+    }
+    void AdjustmentPosition()
+    {
+        transform.RotateAround(_player.transform.position, Vector3.up, _rotate);
+        var rot = Quaternion.Euler(new Vector3(0,_rotate,0));
+        _offset = rot * _offset;
     }
 }
--- C`lock/Assets/Script/PlayerControl.cs
+++ C`lock/Assets/Script/PlayerControl.cs
@@ -2,83 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum PlayerState
-{
-    run,
-    attack,
-    down,
-    idle,
-    max
-}
-
 // プレイヤーの基底クラス
 // Moveなどはここで
 public abstract class PlayerControl : MonoBehaviour
 {
-    // 連想配列 
-    // @key:プレイヤーの状態
-    // @object:アニメーションの名前
-    private Dictionary<PlayerState, string> _animName;
+    private Dictionary<int, string> _animName;
     protected Animator _animator;
-    private GameObject _obj;        // 基底クラスのオブジェクトの情報
-    private PlayerControl _player;  // プレイヤーの情報
-    private PlayerState _state;     // プレイヤーの状態
+    private GameObject _obj;                // 基底クラスのオブジェクトの情報
+    private PlayerControl _player;          // プレイヤーの情報
+    private delegate void StateMethod();    // 状態ごとの処理を行うデリゲート
+    StateMethod State;
+    Animator _anim;
+    private Vector3 _dir;       // 動く向き
+
+
 
     protected void Start(GameObject obj,PlayerControl player,Animator anim)
     {
+        State = Idle;
+        _anim = GetComponent<Animator>();
+        
         _obj = obj;
         _player = player;
         _animator = anim;
-        _animName = new Dictionary<PlayerState, string>();
-        var name = new string[(int)PlayerState.max];
-        name[0] = "IsRun";
-        name[1] = "Jab";
-        name[2] = "DamageDown";
-        name[2] = "DamageDown"; 
-        for (var i = (PlayerState)0;i < PlayerState.max;i++)
+        _animName = new Dictionary<int, string>();
+        // 状態ごとのアニメーション遷移に使う名前
+        var name = new string[_anim.parameters.Length];
+        for (var i = 0;i < _anim.parameters.Length;i++)
         {
-            _animName.Add(i,name[(int)i]);
+            _animName.Add(i, _anim.parameters[(int)i].name);
         }
     }
 
     private void Update()
     {
-        Vector3 vec = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        _dir = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
         if (Input.GetButtonUp("Skill"))
         {
             _player.Skill();
         }
-        if (vec.magnitude != 0)
+        if (_dir.magnitude != 0)
         {
-            Move(vec);
+            Move();
         }
         else
         {
-            // 現在は止まっている状態で範囲内に敵がいれば攻撃する
-            Attack();
         }
-        _player._animator.SetBool(_animName[PlayerState.run], vec.magnitude != 0);       
+        State();
+        _player._animator.SetBool(_animName[0], _dir.magnitude != 0);       
     }
 
-    private void CameraMove()
+    //////ここから状態ごとのメソッド
+    private void Idle()
     {
 
     }
 
     private void Attack()
     {
-
+        int a = 0;
     }
 
     // プレイヤーのアクションメソッド
     // 移動
-    public void Move(Vector3 speed)
+    private void Move()
     {
-        var targetRot  = Quaternion.LookRotation(speed);
+        var targetRot  = Quaternion.LookRotation(_dir);
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRot, Time.deltaTime * 10);
-        _obj.transform.position += speed / 20f;
-        _player._animator.SetBool(_animName[PlayerState.run], true);
-        _player._animator.SetFloat("Speed", speed.z);
+        _obj.transform.position += _dir / 20f;
+        _player._animator.SetBool(_animName[0], true);
+        _player._animator.SetFloat("Speed", _dir.z);
     }
     // 固有スキル使用
     public abstract void Skill();
